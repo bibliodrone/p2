4f7ec3afaa9f32e1ba4acca6bf6599abf69029c6
Gerry Walden <gerald.walden@gmail.com>
2019-02-24 23:04:49 -0500

    Prevent call of Convert  if errors exist from Form.php

--- logic.php
+++ logic.php
@@ -6,7 +6,7 @@ $unitType = $_GET["unitType"] ?? null;
 $valueToConvert = $_GET["valueToConvert"] ?? null;
 $system = $_GET["system"] ?? null;
 
-if(isset($unitType)and isset($valueToConvert) and isset($system)) {
+if(isset($unitType)and isset($valueToConvert) and isset($system) and !($errors)) {
     $returnMessage = $convert->convert($system, $unitType, $valueToConvert);
 }
 
